3ac542d89f5dcf502f27ae77db46d90a6a0923b6
Blazercs <51568874+Blazercs@users.noreply.github.com>
2019-06-10 12:39:49 +0300

    Create randomProf.cs

---
+++ randomProf.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class randomProf : MonoBehaviour {
+	public int rand = 0;
+	// Use this for initialization
+	void Rand () {
+		rand = Random.Range (0, 3);
+	}
+	
+	// Update is called once per frame
+	void Start () {
+		Rand ();
+	}
+	void Update(){
+		if (GameObject.FindGameObjectWithTag ("Voenn")) {
+
+			rand = Random.Range(1,3);
+
+				}
+		if (GameObject.FindGameObjectWithTag ("Medik")) {
+			
+			rand = Random.Range(0,3);
+			
+		}
+		if (GameObject.FindGameObjectWithTag ("Meh")) {
+			
+			rand = Random.Range(0,2);
+			
+		}
+
+	}
+}
